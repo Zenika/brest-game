402ea1a326847418d4a54a4dd157fba3b419934f
Antoine Duchenet <antoine.duchenet@gmail.com>
2025-04-01 09:13:21 +0200

    🧑‍💻: enable Bevy dev tools

--- game_client/src/main.rs
+++ game_client/src/main.rs
@@ -5,6 +5,7 @@ use anima::AnimaPlugin;
 use bevy::{
     color::palettes::tailwind::{GRAY_50, ROSE_600, ROSE_800, TEAL_50},
     prelude::*,
+    window::PresentMode,
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
@@ -14,10 +15,27 @@ use scene::ScenePlugin;
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: (1600., 900.).into(),
+                    present_mode: PresentMode::AutoNoVsync,
+                    ..default()
+                }),
+                ..default()
+            }),
+            AnimaPlugin,
             MeshPickingPlugin,
             WorldInspectorPlugin::new(),
-            AnimaPlugin,
+            bevy::dev_tools::fps_overlay::FpsOverlayPlugin {
+                config: bevy::dev_tools::fps_overlay::FpsOverlayConfig {
+                    text_config: TextFont {
+                        font_size: 16.0,
+                        ..default()
+                    },
+                    text_color: bevy::color::palettes::css::WHITE.into(),
+                    enabled: true,
+                },
+            },
         ))
         .add_plugins(ScenePlugin {
             key_light_illuminance: light_consts::lux::OVERCAST_DAY,
